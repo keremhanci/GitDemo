5aad19e5c04f00a61c436dff61d048fc5028c9ed
keremhanci <kerem_hanci@hotmail.com>
2020-04-27 12:58:23 +0300

    first commit

---
+++ src/test/java/Works/AppiumTest.java
@@ -0,0 +1,28 @@
+package Works;
+
+
+import org.testng.annotations.Test;
+
+import junit.framework.Assert;
+
+
+
+
+public class AppiumTest {
+	
+	@Test
+	public void nativeAppAndroid()
+	{
+		System.out.println("nativeAppAndroid");
+	}
+	
+	@Test
+	public void IOSApps()
+	{
+		Assert.assertTrue(false);
+		System.out.println("IOSApps");
+		
+	}
+
+
+}
